252b06f7239136b93c7f0e1f100ca74e5d9e8b25
Dima Pinaev <dimapokemon005@gmail.com>
2023-03-24 21:11:29 +0300

    homework-7 добавил логирование

--- src/notebook/controller/UserController.java
+++ src/notebook/controller/UserController.java
@@ -1,5 +1,6 @@
 package notebook.controller;
 
+import notebook.log.Logger;
 import notebook.model.User;
 import notebook.repository.GBRepository;
 
@@ -8,28 +9,32 @@ import java.util.Objects;
 
 public class UserController {
     private final GBRepository<User, Long> repository;
+    private Logger logger;
 
-    public UserController(GBRepository<User, Long> repository) {
+    public UserController(GBRepository<User, Long> repository, Logger logger) {
         this.repository = repository;
+        this.logger = logger;
     }
 
     public void saveUser(User user) {
         repository.create(user);
+        logger.log("Создал нового пользователя: " +
+                user.getId() + " " + user.getFirstName() + " " +
+                user.getLastName() + " " + user.getPhone());
     }
 
     public User readUser(Long userId) throws Exception {
         List<User> users = repository.findAll();
         for (User user : users) {
             if (Objects.equals(user.getId(), userId)) {
-               return user;
+                return user;
             }
         }
-
         throw new RuntimeException("User not found");
     }
 
     public User findUserById(long id) {
-        return repository.findById(id).orElseThrow(()-> new RuntimeException("User not found."));
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("User not found."));
     }
 
     public List<User> getAllUsers() {
@@ -39,6 +44,8 @@ public class UserController {
     public boolean userUpdate(long id, User update) {
         try {
             repository.update(id, update);
+            logger.log("Обновлённый пользователь: " + update.getFirstName() + " " + 
+                update.getLastName() + " " + update.getPhone());
             return true;
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -47,6 +54,7 @@ public class UserController {
 
     public boolean userRemove(Long id) {
         try {
+            logger.log("Удалил пользователя с id: " + id);
             return repository.delete(id);
         } catch (Exception e) {
             throw new RuntimeException(e);
---
+++ src/notebook/log/impl/ConsoleLogger.java
@@ -0,0 +1,12 @@
+package notebook.log.impl;
+
+import java.time.LocalDateTime;
+
+import notebook.log.Logger;
+
+public class ConsoleLogger implements Logger {
+    @Override
+    public void log(String text) {
+        System.err.println(LocalDateTime.now() + ": " + text);
+    }
+}
